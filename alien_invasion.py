14360ece7c1c9b892829e358573bde53b3511c9c
Lomovskoy <lomovskoy.kirill@yandex.ru>
2017-09-03 19:43:47 +0300

    Сделанно проигрывание и 3 жизни.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -13,6 +14,9 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,
                                       ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    
+    # Создание экземпляра для хранения игровой статистики.
+    stats = GameStats(ai_settings)
 
     # Создание корабля.
     ship = Ship(ai_settings, screen)
@@ -28,20 +32,21 @@ def run_game():
         
         # Отслеживание событий клавиатуры и мыши.
         gf.check_events(ai_settings, screen, ship, bullets)
-        
-        #Обновление позиции корабля и пули
-        ship.update()
-        bullets.update()
 
-        # Удаление пуль, вышедших за край экрана.
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        
-        #Обновление пришельцев
-        gf.update_aliens(ai_settings, aliens)
+        if stats.game_active:
+            #Обновление позиции корабля и пули
+            ship.update()
+            bullets.update()
+
+            # Удаление пуль, вышедших за край экрана.
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            
+            #Обновление пришельцев
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         
         # Отображение последнего прорисованного экрана.
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
-#270
+#281
--- settings.py
+++ settings.py
@@ -11,7 +11,8 @@ class Settings():
 
         # Настройки корабля
         self.ship_speed_factor = 1.5
-
+        self.ship_limit = 3
+        
         # Параметры пули
         self.bullet_speed_factor = 1
         self.bullet_width = 3
